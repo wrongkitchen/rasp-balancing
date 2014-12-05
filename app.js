a71a670f9af0f0ec7d3c777d14c02f25586dbcfe
wrongkitchen <wkc.kenji@gmail.com>
2014-12-05 12:40:37 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+var app = require('express')();
+var server = require('http').Server(app)
+var io = require('socket.io')(server);
+var i2c = require('i2c');
+var address = 0x68;
+var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+var getPosition = function(pCallback){
+        var x = null, y = null, z = null;
+        var read_word_2c = function(adr, cCallback){
+                var high = null;
+                var low = null;
+                var byteCal = function(){
+                        var tmpVal = (high << 8) + low;
+                        var val = null;
+                        if(tmpVal >= 0x8000)
+                                val = -((65535 - tmpVal) + 1);
+                        else
+                                val = tmpVal;
+                        cCallback(val / 16384.0);
+                };
+                wire.readBytes(adr, 8, function(err, res) {
+                        high = res.readUInt8(0);
+                        if(high && low) byteCal();
+                });
+                wire.readBytes(adr+1, 8, function(err, res) {
+                        low = res.readUInt8(0);
+                        if(high && low) byteCal();
+                });
+        };
+        var cal = function(){
+                var pos = {};
+                var dist = function(a,b){
+                        return Math.sqrt((a*a)+(b*b));
+                };
+                var degrees = function(rad){
+                        return rad*(180/Math.PI);
+                };
+                var get_x = function(){
+                        return Math.atan2(y, dist(x,z));
+                };
+                var get_y = function(){
+                        return Math.atan2(x, dist(y,z)) * -1;
+                };
+                pos.x = degrees(get_x());
+                pos.y = degrees(get_y());
+                pCallback(pos);
+        };
+        read_word_2c(0x3b, function(pVal){
+                x = pVal;
+                if(x && y && z) cal();
+        });
+        read_word_2c(0x3d, function(pVal){
+                y = pVal;
+                if(x && y && z) cal();
+        });
+        read_word_2c(0x3f, function(pVal){
+                z = pVal;
+                if(x && y && z) cal();
+        });
+};
+
+app.listen(3000);
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+wire.writeByte(0x6b, function(err){
+	setInterval(function(){
+		getPosition(function(pPos){
+			io.on('connection', function (socket) {
+				socket.emit('passXY', pPos);
+			});
+		});
+	}, 100);
+});
---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	var smoothie = new SmoothieChart({
+		maxValue : -90,
+		minValue : 90
+	});
+		smoothie.streamTo(document.getElementById("mycanvas"));
+	// Data
+	var line1 = new TimeSeries();
+	var line2 = new TimeSeries();
+	var getXYdata = function(){
+		$.ajax({
+	        url: '//192.168.1.141:8080',
+	        type: 'get',
+	        dataType: "json",
+	        success: function (data) {
+				line1.append(new Date().getTime(), data.x);
+				line2.append(new Date().getTime(), data.y);
+				getXYdata();
+	        }
+	    });
+	};
+	// Add to SmoothieChart
+	smoothie.addTimeSeries(line1, { strokeStyle: 'rgba(255, 255, 0,1)', fillStyle: 'rgba(255, 255, 0, 0.1)', lineWidth: 1 });
+	smoothie.addTimeSeries(line2, { strokeStyle: 'rgba(255, 0, 0,1)', fillStyle: 'rgba(255, 0, 0, 0.1)', lineWidth: 1 });
+	getXYdata();
+});
