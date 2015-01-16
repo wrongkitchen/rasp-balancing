d00a3b4b12d5136c8f969dd28bf15f07e3445483
wrongkitchen <wkc.kenji@gmail.com>
2015-01-16 18:07:23 +0800

    added socket.io framework

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app)
-var io = require('socket.io')(server);
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server);
+var device  = require('express-device');
+var runningPortNumber = process.env.PORT;
+
 var i2c = require('i2c');
 var address = 0x68;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
@@ -60,12 +63,47 @@ var getPosition = function(pCallback){
         });
 };
 
-app.listen(3000);
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-    res.sendfile(__dirname + '/public/index.html');
+
+app.configure(function(){
+    // I need to access everything in '/public' directly
+    app.use(express.static(__dirname + '/public'));
+
+    //set the view engine
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname +'/views');
+
+    app.use(device.capture());
 });
+
+
+// logs every request
+app.use(function(req, res, next){
+    // output every request in the array
+    console.log({method:req.method, url: req.url, device: req.device});
+
+    // goes onto the next function in line
+    next();
+});
+
+app.get("/", function(req, res){
+    res.render('index', {});
+});
+
+io.sockets.on('connection', function (socket) {
+
+    io.sockets.emit('blast', {msg:"<span style=\"color:red !important\">someone connected</span>"});
+
+    socket.on('blast', function(data, fn){
+        console.log(data);
+        io.sockets.emit('blast', {msg:data.msg});
+
+        fn();//call the client back to clear out the field
+    });
+
+});
+
+server.listen(runningPortNumber);
 
 wire.writeByte(0x6b, function(err){
 	setInterval(function(){
